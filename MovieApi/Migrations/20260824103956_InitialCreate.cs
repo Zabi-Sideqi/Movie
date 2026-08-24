a700d5d2284c9eea752050c07dea3b0d6ce28e4a
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-08-24 13:05:17 +0200

    Add project files.

---
+++ MovieApi/Migrations/20260824103956_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: false),
+                    DurationMinutes = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+
+        public string Title { get; set; }
+        [Required]
+        [StringLength(100)]
+
+        public string Genre { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Director { get; set; }
+        [Range(1900, 2100)]
+        public int ReleaseYear { get; set; }
+        [Range(1, 500)]
+        public int DurationMinutes { get; set; }
+        [Range(0.0, 10.0)]
+        public double Rating { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
